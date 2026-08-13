7eda16660192036a8a511298a2213ab3ae885dc3
Fede1109 <federicojose2000@gmail.com>
2026-08-13 20:31:02 +0200

    feat: null checker

---
+++ examples/ex05.rs
@@ -0,0 +1,36 @@
+use matrix::Vector;
+
+pub fn angle_cos(u: &Vector<f32>, v: &Vector<f32>) -> f32 {
+    if u.norm() == 0. || v.norm() == 0. {
+        panic!("Cannot compute angle with zero vector");
+    }
+    if u.len() != v.len() {
+        panic!("Vectors must have the same length");
+    }
+
+    let norm_product = u.norm() * v.norm();
+    u.dot(v) / norm_product
+}
+
+pub fn main(){
+    let u = Vector::from([1., 0.]);
+    let v = Vector::from([1., 0.]);
+    println!("{}", angle_cos(&u, &v));
+    // 1.0
+    let u = Vector::from([1., 0.]);
+    let v = Vector::from([0., 1.]);
+    println!("{}", angle_cos(&u, &v));
+    // 0.0
+    let u = Vector::from([-1., 1.]);
+    let v = Vector::from([ 1., -1.]);
+    println!("{}", angle_cos(&u, &v));
+    // -1.0
+    let u = Vector::from([2., 1.]);
+    let v = Vector::from([4., 2.]);
+    println!("{}", angle_cos(&u, &v));
+    // 1.0
+    let u = Vector::from([1., 2., 3.]);
+    let v = Vector::from([4., 5., 6.]);
+    println!("{}", angle_cos(&u, &v));
+    // 0.974631846
+}

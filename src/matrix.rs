599792221e717d2ef26fc8f4c847206bd20a3566
Fede1109 <federicojose2000@gmail.com>
2026-08-14 13:34:42 +0200

    feat: ex07 & ex08

--- src/matrix.rs
+++ src/matrix.rs
@@ -1,5 +1,6 @@
 use std::ops::{Add, Sub, Mul, AddAssign, SubAssign, MulAssign};
 use std::fmt;
+use crate::Vector;
 
 #[derive(Clone, Debug)]
 pub struct Matrix<K> {
@@ -16,6 +17,65 @@ impl<K> Matrix <K>
 
     pub fn new(data: Vec<Vec<K>>)  -> Self{
         Self { data }
+    }
+
+   pub fn mul_vec(&self, vec: &Vector<K>) -> Vector<K>
+    where
+        K: Copy + Mul<Output = K> + AddAssign + Default, 
+    {
+        let cols = if self.data.is_empty() { 0 } else { self.data[0].len() };
+        if cols != vec.data.len() {
+            panic!("Matrix columns must match vector length");
+        }
+        let mut result = Vec::new();
+        for row in &self.data {
+            let mut sum = K::default();
+            for (&val, &v_val) in row.iter().zip(vec.data.iter()) {
+                sum += val * v_val;
+            }
+            result.push(sum);
+        }
+        Vector::new(result)
+    }
+
+    pub fn mul_mat(&self, mat: &Matrix<K>) -> Matrix<K>
+    where
+        K: Copy + Mul<Output = K> + AddAssign + Default,
+    {
+        let (rows_a, cols_a) = self.shape();
+        let (rows_b, cols_b) = mat.shape();
+        if cols_a != rows_b {
+            panic!("Matrix A columns must match Matrix B rows");
+        }
+        let mut result = Vec::with_capacity(rows_a);
+
+        for i in 0..rows_a {
+            let mut new_row = Vec::with_capacity(cols_b);
+            for j in 0..cols_b {
+                let mut sum = K::default();
+                for k in 0..cols_a {
+                    sum += self.data[i][k] * mat.data[k][j];
+                }
+                new_row.push(sum);
+            }
+            result.push(new_row);
+        }
+        Matrix::new(result)
+    }
+
+    pub fn trace(&self) -> K
+    where
+        K: Copy + AddAssign + Default,
+    {
+        let (rows, cols) = self.shape();
+        if rows != cols {
+            panic!("Matrix must be square");
+        }
+        let mut sum = K::default();
+        for i in 0..rows {
+            sum += self.data[i][i];
+        }
+        sum
     }
 
 }
